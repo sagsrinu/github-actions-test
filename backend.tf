8182583f77897eb87b132fda06d8a8446ed2214f
sagsrinu <sagsrinu@gmail.com>
2026-04-05 11:54:17 +0530

    Rename S3 bucket for Terraform backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "github_backend05042026"
+    bucket         = "githubbackend05042026"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
